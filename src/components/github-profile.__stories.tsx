51a9152be65fc5de2d7e8870a12659542c9a85c4
e9aru <hi@e9aru.com>
2020-08-12 20:33:56 +0200

    add stories draft

---
+++ src/components/github-profile.__stories.tsx
@@ -0,0 +1,5 @@
+import React from "react"
+import { storiesOf } from "@storybook/react"
+import GithubProfile from "./github-profile"
+
+storiesOf("Other|GithubProfile", module).add("main", () => <GithubProfile />)
